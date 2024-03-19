d6bffe2c2f66d7d81a9616c97bc89273ecbdd9c3
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-03-19 20:47:38 +0300

    CategoryCounter add total square in category

---
+++ CategoryCounterValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReading
+{
+    internal class CategoryCounterValues
+    {
+        public float Value;
+        public float Square;
+
+        public CategoryCounterValues(float value, float square)
+        {
+            Value = value;
+            Square = square;
+        }
+    }
+}
--- ElementsCategoriesCounter.cs
+++ ElementsCategoriesCounter.cs
@@ -4,40 +4,42 @@ namespace ExcelReading
 {
     internal class ElementsCategoriesCounter
     {
-        public Dictionary<string, float> CategoriesCount;
+        public Dictionary<string, CategoryCounterValues> CategoriesCount;
         public ElementsCounter ElementsCounter { get; private set; }
 
         public ElementsCategoriesCounter(ElementsCounter elementsCounter)
         {
-            CategoriesCount = new Dictionary<string, float>();
+            CategoriesCount = new Dictionary<string, CategoryCounterValues>();
             ElementsCounter = elementsCounter;
         }
 
         public void CountElements()
         {
-            foreach(KeyValuePair<Element, float> kvp in ElementsCounter.ElementsCounts) 
+            foreach (KeyValuePair<Element, float> kvp in ElementsCounter.ElementsCounts)
             {
                 string newCategory;
                 newCategory = kvp.Key.Category;
 
-                if(CategoriesCount.ContainsKey(newCategory))
+                if (CategoriesCount.ContainsKey(newCategory))
                 {
-                    CategoriesCount[newCategory] += kvp.Value;
+                    CategoriesCount[newCategory].Value += kvp.Value;
+                    CategoriesCount[newCategory].Square += kvp.Key.Square;
                 }
-                else 
-                { 
-                    CategoriesCount.Add(newCategory, kvp.Value); 
+                else
+                {
+                    CategoriesCount.Add(newCategory, new CategoryCounterValues(kvp.Value, kvp.Key.Square));
                 }
             }
         }
 
         public void PrintInfo()
         {
-            foreach (KeyValuePair<string, float> keyValuePair in CategoriesCount)
+            foreach (KeyValuePair<string, CategoryCounterValues> keyValuePair in CategoriesCount)
             {
-                Console.Write($"Категория {keyValuePair.Key} в количестве:{keyValuePair.Value}");
+                Console.Write($"Категория {keyValuePair.Key} в количестве:{keyValuePair.Value.Value}, " +
+                    $"общей площадью: {keyValuePair.Value.Square} м2");
 
-                if (keyValuePair.Value == 0)
+                if (keyValuePair.Value.Value == 0 || keyValuePair.Value.Square == 0)
                     Console.Write("<==========> NULL VALUE <============>");
 
                 Console.WriteLine();
